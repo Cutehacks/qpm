abfd26771d7429a8d14622a1cade5eeafdc19b6c
Marius Bugge Monsen <marius@cutehacks.com>
2015-10-05 11:07:47 +0200

    Change the macx package install directory to /usr/local/bin

--- osx/build_pkg.sh
+++ osx/build_pkg.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mkdir ./dstroot
 install -m 755 ../bin/qpm ./dstroot/
-pkgbuild --identifier io.qpm --root ./dstroot/ --install-location /opt/local/bin qpm.pkg
+pkgbuild --identifier io.qpm --root ./dstroot/ --install-location /usr/local/bin qpm.pkg
